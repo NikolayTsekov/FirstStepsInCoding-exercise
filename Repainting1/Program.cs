09745ce23c75f0f643c47a51ff343269bb309461
NikolayTsekov <niki.cekov93@gmail.com>
2023-04-28 14:08:06 +0300

    variables

---
+++ Repainting1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nylon = int.Parse(Console.ReadLine());
+            int paint = int.Parse(Console.ReadLine());
+            int thinner = int.Parse(Console.ReadLine());
+            int hours = int.Parse(Console.ReadLine());
+
+            double nylonPrice = (nylon + 2) * 1.5;
+            double paintPrice = (paint + 1.1) * 14.5;
+            double thinnerPrice = thinner * 5;
+            double bagsPrice = 0.4;
+
+
+            double materialPrice = nylonPrice + paintPrice + thinnerPrice + bagsPrice;
+
+            double masterPrice = (materialPrice * 0.3) * 8;
+            double totalPrice = materialPrice + masterPrice;
+
+            Console.WriteLine(totalPrice);
+        }
+    }
+}
+
+
+
