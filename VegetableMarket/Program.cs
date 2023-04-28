5bc26908b267fb13b6d15a4366d5fbf72265d278
NikolayTsekov <niki.cekov93@gmail.com>
2023-04-28 18:23:01 +0300

    variables

---
+++ VegetableMarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vegetable = double.Parse(Console.ReadLine());
+            double fruits = double.Parse(Console.ReadLine());   
+            int vegetableTotalKilograms = int.Parse(Console.ReadLine());
+            int fruitsTotalKilograms = int.Parse(Console.ReadLine());
+            
+            double vegetablePrice = vegetable * vegetableTotalKilograms;
+            double fruitsPrice = fruits * fruitsTotalKilograms;   
+            double total = (vegetablePrice + fruitsPrice) / 1.94;
+
+            Console.WriteLine($"{total:F2}");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
