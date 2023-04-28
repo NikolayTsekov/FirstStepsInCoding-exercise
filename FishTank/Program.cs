413552ff8c67208fcfb416ffd4f9261336b02d31
NikolayTsekov <niki.cekov93@gmail.com>
2023-04-28 14:54:59 +0300

    variables

---
+++ FishTank/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double percent = double.Parse(Console.ReadLine());
+
+            double aquariumCapacity = lenght * width * height;
+            double capacityInLitres = aquariumCapacity * 0.001;
+            double occupiedSpace = 0.185;
+
+            double neededLitres = capacityInLitres * (1 - 0.185);
+
+            Console.WriteLine(neededLitres);
+        }
+    }
+}
