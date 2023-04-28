4e884545e4e3c8931a6b4aa2c5ba94f1b52946c9
NikolayTsekov <niki.cekov93@gmail.com>
2023-04-28 17:44:00 +0300

    variables

---
+++ CelsiusToFahrenheit/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius = double.Parse(Console.ReadLine());
+
+            double fahrenheit = celsius * 1.8 + 32;
+
+            Console.WriteLine($"{fahrenheit:F2}");
+        }
+    }
+}
+
+
+
+
+
+
+
