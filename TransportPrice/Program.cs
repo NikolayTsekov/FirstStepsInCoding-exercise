99d3c5e0e4316123632b0d4221418b4485daad21
NikolayTsekov <niki.cekov93@gmail.com>
2023-05-04 19:40:24 +0300

    if/else

---
+++ TransportPrice/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kilometers = int.Parse(Console.ReadLine());
+            string tariff = Console.ReadLine();
+
+            double taxiStartPrice = 0.70;
+            double taxiDailyPrice = 0.79;
+            double taxiNightPrice = 0.90;
+            double busDailyNightPrice = 0.09;
+            double trainDailyNightPrice = 0.06;
+
+            if (kilometers < 20)
+            {
+                if (tariff == "day")
+                {
+                    double taxiPrice = taxiStartPrice + (kilometers * taxiDailyPrice);
+                    Console.WriteLine($"{Math.Abs(taxiPrice):f2}");
+               
+                }
+                else
+                {
+                    double taxiPrice = taxiStartPrice + (kilometers * taxiNightPrice);
+                    Console.WriteLine($"{Math.Abs(taxiPrice):f2}");
+                }
+                
+            }
+            else if (kilometers >= 100)
+            {
+                double trainPrice = kilometers * trainDailyNightPrice;
+                Console.WriteLine($"{Math.Abs(trainPrice):f2}");
+                
+            }
+            else 
+            {
+                double busPrice = kilometers * busDailyNightPrice;
+                Console.WriteLine($"{Math.Abs(busPrice):f2}");
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
