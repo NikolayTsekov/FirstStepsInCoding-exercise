e0adb78cac0392d084f5ea6e5716c88021da4033
NikolayTsekov <niki.cekov93@gmail.com>
2023-05-14 10:26:31 +0300

    if/else

---
+++ Cinema/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Data;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*В една кинозала столовете са наредени в правоъгълна форма в r реда и c колони. Има три вида прожекции с билети на различни цени:
+	•	Premiere – премиерна прожекция, на цена 12.00 лева.
+	•	Normal – стандартна прожекция, на цена 7.50 лева.
+	•	Discount – прожекция за деца, ученици и студенти на намалена цена от 5.00 лева.
+Напишете програма, която чете тип прожекция (стринг), брой редове и брой колони в залата (цели числа), въведени от потребителя, и изчислява общите приходи от билети при пълна зала. Резултатът да се отпечата във формат като в примерите по-долу, с 2 знака след десетичната точка.  */
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int colums = int.Parse(Console.ReadLine());
+
+            double income = 0.0;
+            if (type == "Premiere")
+            {
+                income = rows * colums * 12.00;
+            }
+            else if (type == "Normal")
+            {
+                income = rows * colums * 7.50;
+            }
+            else if (type == "Discount")
+            {
+                income = rows * colums * 5.00;
+            }
+            Console.WriteLine($"{income:F2} leva");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
