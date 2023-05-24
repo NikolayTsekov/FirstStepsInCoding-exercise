38b42db7d0c0e20de4e79ef5de1ce3d621fb9783
NikolayTsekov <niki.cekov93@gmail.com>
2023-05-24 15:16:41 +0300

    commit

---
+++ ForLoopExercise/Equal Pairs/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Дадени са 2*n-на брой числа. Първото и второто формират двойка, третото и четвъртото също и т.н. Всяка двойка има стойност – сумата от съставящите я числа. Напишете програма, която проверява дали всички двойки имат еднаква стойност или печата максималната разлика между две последователни двойки. Ако всички двойки имат еднаква стойност, отпечатайте "Yes, value={Value}" + стойността. В противен случай отпечатайте "No, maxdiff={Difference}" + максималната разлика. */
+            int n = int.Parse(Console.ReadLine());
+            int sumNewPair = 0;
+            int sumOldPair = 0;
+            int maxDiff = 1;
+            int counter = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                int f = int.Parse(Console.ReadLine());
+                int g = int.Parse(Console.ReadLine());
+                sumNewPair = f + g;
+                if (sumNewPair == sumOldPair)
+                {
+                    counter += 1;
+                }
+                if (maxDiff < Math.Abs(sumOldPair - sumNewPair) && i >= 2)
+                    maxDiff = Math.Abs(sumOldPair - sumNewPair);
+                if (sumNewPair != sumOldPair)
+                {
+                    sumOldPair = sumNewPair;
+                }
+            }
+            if (counter == n - 1)
+                Console.WriteLine("Yes, value={0}", sumNewPair);
+            else
+                Console.WriteLine("No, maxdiff={0}", maxDiff);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
