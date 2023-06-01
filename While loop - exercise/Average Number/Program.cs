57a1ab243200be10478a987ca227a4b71222c268
NikolayTsekov <niki.cekov93@gmail.com>
2023-06-01 18:51:44 +0300

    initial commit

---
+++ While loop - exercise/Average Number/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Напишете програма, която прочита едно число n, след това прочита n на брой цели числа и принтира средно аритметичното на тяхната сума число, форматирано до втората цифра след десетични знак.*/
+            int n = int.Parse(Console.ReadLine());
+            int dupf = n;
+            double sum = 0;
+
+            while (dupf != 0)
+            {
+                dupf = dupf - 1;
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+            Console.WriteLine($"{sum / n:F2}");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ While loop - exercise/Numbers Divided by 3 Without Reminder1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Напишете програма, която печата всички числа в интервала от 1 до 100, който се делят на 3 без остатък, по едно на ред.*/
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
