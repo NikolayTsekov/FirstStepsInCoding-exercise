a32ae79ef08a3da859e2c0ed557e3f140e35d0f1
NikolayTsekov <niki.cekov93@gmail.com>
2023-06-03 13:59:17 +0300

    initial commit

---
+++ Nested Loop - exercise/Number Pyramid/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int n = int.Parse(Console.ReadLine());
+           int current = 1;
+           bool isBigger = false;
+
+            for (int rows = 1; rows <= n; rows++)
+            {
+                for (int column = 1; column <= rows; column++)
+                {
+                    if (current > n)
+                    {
+                        isBigger = true;
+                        break;
+                    }
+                    Console.Write(current + " ");
+                    current++;
+                }
+                if (isBigger) 
+                {
+                    break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
