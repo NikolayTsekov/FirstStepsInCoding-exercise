84144042c561d5df8f2cd4f537164d3094c5089b
NikolayTsekov <niki.cekov93@gmail.com>
2023-06-05 11:28:15 +0300

    commit

---
+++ Nested Loop - exercise/Stars Pyramid/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FirstStepsInCoding_22_04_2023
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int row = 1; row <= n; row++)
+            {
+                for (int column = 1; column <= row; column++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
